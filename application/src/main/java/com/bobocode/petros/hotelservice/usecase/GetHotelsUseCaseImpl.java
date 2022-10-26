bb75527824ca625086e8cc356b5bbbcfb9cb45ee
Kirill Kotenok <kirill.kotenok@data-ox.com>
2022-10-26 20:49:49 +0300

    Fixed dependencies problem

--- application/src/main/java/com/bobocode/petros/hotelservice/usecase/GetHotelsUseCaseImpl.java
+++ application/src/main/java/com/bobocode/petros/hotelservice/usecase/GetHotelsUseCaseImpl.java
@@ -1,15 +1,15 @@
 package com.bobocode.petros.hotelservice.usecase;
 
 import com.boboode.petros.hotelservice.adapter.HotelGeneratorAdapter;
-import com.boboode.petros.hotelservice.entity.Hotel;
 import com.boboode.petros.hotelservice.entity.ClientHotelRequest;
+import com.boboode.petros.hotelservice.entity.Hotel;
 import com.boboode.petros.hotelservice.usecase.GetHotelsUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class GetHotelsUseCaseImpl implements GetHotelsUseCase {
     private final HotelGeneratorAdapter adapter;
